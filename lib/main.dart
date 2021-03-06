18aea3e844dfde6d92c9bdd68e7b6440b194bdd3
busatoju <busatoju@gmail.com>
2021-03-06 19:36:43 -0300

    Chamada do app - runApp

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:conversor_moedas/views/home_view.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(AppWidget());
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: HomeView(),
+      
+    );
+  }
+}
