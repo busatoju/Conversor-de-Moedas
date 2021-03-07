e951f3b733bf5431e1458665b8355bfeaea92a74
busatoju <busatoju@gmail.com>
2021-03-07 09:01:16 -0300

    Correção do import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:conversor_moedas/views/home_view.dart';
+
+import 'package:conversor_moedas/app/views/home_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
