168eb0b91d5bbb5bb7f31d355637e93ac521b892
busatoju <busatoju@gmail.com>
2021-03-07 11:45:09 -0300

    setState implementado

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -1,8 +1,25 @@
-
 import 'package:conversor_moedas/app/components/currency_box.dart';
+import 'package:conversor_moedas/app/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final TextEditingController toText = TextEditingController();
+  final TextEditingController fromText = TextEditingController();
+
+  HomeController homeController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    homeController = HomeController(toText: toText, fromText: fromText);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,14 +40,34 @@ class HomeView extends StatelessWidget {
               SizedBox(
                 height: 50,
               ),
-              CurrencyBox(),
-              CurrencyBox(),
+              CurrencyBox(
+                selectedItem: homeController.toCurrency,
+                controller: toText,
+                items: homeController.currencies,
+                onChanged: (model) {
+                  setState(() {
+                    homeController.toCurrency = model;
+                  });
+                },
+              ),
+              CurrencyBox(
+                selectedItem: homeController.fromCurrency,
+                controller: fromText,
+                items: homeController.currencies,
+                onChanged: (model) {
+                  setState(() {
+                    homeController.fromCurrency = model;
+                  });
+                },
+              ),
               SizedBox(
                 height: 30,
               ),
               RaisedButton(
                 color: Colors.amber,
-                onPressed: () {},
+                onPressed: () {
+                  homeController.convert();
+                },
                 child: Text('CONVERTER'),
               )
             ],
