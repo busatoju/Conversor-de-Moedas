e304ba834ff47e2c58771aea8b089e7d60083cab
busatoju <busatoju@gmail.com>
2021-03-07 09:03:40 -0300

    Implementada controller e regra de negócio do app

---
+++ lib/app/controllers/home_controller.dart
@@ -0,0 +1,35 @@
+import 'package:conversor_moedas/app/models/currency_model.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomeController {
+  List<CurrencyModel> currencies;
+
+  TextEditingController toText;
+  TextEditingController fromText;
+
+  CurrencyModel toCurrency;
+  CurrencyModel fromCurrency;
+
+  HomeController({this.toCurrency,this.fromCurrency}) {
+    currencies = CurrencyModel.getCurrencies();
+    toCurrency = currencies[0];
+    fromCurrency = currencies[1];
+  }
+
+  void convert() {
+    String text = toText.text;
+    double value = double.tryParse(text.replaceAll(',','.')) ?? 1.0;
+    double returnValue = 0;
+
+    if (fromCurrency.name == 'Real') {
+      returnValue = value * toCurrency.real;
+    } else if (fromCurrency.name == 'Dolar') {
+      returnValue = value * toCurrency.dolar;
+    } else if (fromCurrency.name == 'Euro') {
+      returnValue = value * toCurrency.euro;
+    } else if (fromCurrency.name == 'Bitcoin') {
+      returnValue = value * toCurrency.bitcoin;
+    }
+    fromText.text = returnValue.toStringAsFixed(2);
+  }
+}
