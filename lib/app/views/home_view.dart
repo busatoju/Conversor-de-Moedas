5882660dbb0166597e427d2bbada7a5a3949113a
busatoju <busatoju@gmail.com>
2021-03-06 22:40:03 -0300

    Adicionada a tela de conversão

---
+++ lib/app/views/home_view.dart
@@ -0,0 +1,41 @@
+import 'package:conversor_moedas/components/currency_box.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 100, bottom: 20, left: 30, right: 30),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/logo.png',
+                width: 150,
+                height: 150,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              CurrencyBox(),
+              CurrencyBox(),
+              SizedBox(
+                height: 30,
+              ),
+              RaisedButton(
+                color: Colors.amber,
+                onPressed: () {},
+                child: Text('CONVERTER'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
